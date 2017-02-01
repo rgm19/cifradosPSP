44e76f9d6c760c28280dbd3abae57970dc7d958a
rgm19 <gmruben1995@gmail.com>
2017-02-01 11:50:23 +0100

    1.1

---
+++ src/CifradoGrafico/Principal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CifradoGrafico;
+
+/**
+ *
+ * @author ruben
+ */
+public class Principal {
+    public static void main(String[]args){
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        
+        ventana.setTitle("Encriptador de Archivo");
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+    }
+}
